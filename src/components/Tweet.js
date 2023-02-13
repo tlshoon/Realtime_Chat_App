b4a7a3bcad5331c8624560535b0392b3ca3cb43d
신재훈 <sjhh0105@naver.com>
2023-02-13 23:48:18 +0900

    v4

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { dbService } from "../fbase";
 
-const Tweet = ({ Tweetobj, isOwner }) => (
+const Tweet = ({ Tweetobj, isOwner }) => {
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("트윗을 지울까요?");
+    console.log(ok)
+    if (ok) {
+      await dbService.doc(`tweets/${Tweetobj.id}`).delete();
+    }
+  }
+  
+  return(
   <div>
     <h4>{Tweetobj.text}</h4>
     {isOwner && (
       <>
-        <button>Delete Button</button>
+        <button onClick={onDeleteClick}>Delete Button</button>
         <button>Edit Button</button>
       </>
     )}
   </div>
-);
+  );
+};
 
 export default Tweet;
